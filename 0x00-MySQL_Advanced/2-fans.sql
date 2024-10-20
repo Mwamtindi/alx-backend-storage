604987c07fab45358f88fb433f4d60d2e9cbf620
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-20 08:52:47 +0300

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -4,6 +4,9 @@
 -- Select origin and the sum of fans for each origin
 -- SQL keywords are in uppercase, the script is commented as per the task req.
 
+CREATE TEMPORARY TABLE tmp_returns (origin VARCHAR(255), nb_fans INT);
+
+INSERT INTO tmp_returns (origin, nb_fans)
 SELECT origin, COUNT(DISTINCT b_name) as nb_fans
 FROM metal_bands
 GROUP BY origin
