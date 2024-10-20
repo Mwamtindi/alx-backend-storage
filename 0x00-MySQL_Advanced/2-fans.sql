db1c6ee89adf6e88be10d334f403174847b51662
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-20 09:11:49 +0300

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,7 @@
 -- Task: Rank country origins of bands based on the number of non-unique fans
 -- This script retrieves and orders band origins by their fan count in descending order.
 
--- Select origin and the sum of fans for each origin
--- SQL keywords are in uppercase, and the script is commented as per the task requirements.
-
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-HAVING SUM(nb_fans) > 0
 ORDER BY nb_fans DESC;
