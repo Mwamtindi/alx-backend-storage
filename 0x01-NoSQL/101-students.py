adcad2b2149c14ce040922f6001e79ba78afd9b3
Mwamtindi <mwamtindishaban@gmail.com>
2024-10-23 20:49:56 +0300

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module provides a func to return all students sorted by their avg score.
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by their average score.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+
+    Returns:
+        A list of student documents sorted by their average score,
+        with each document containing the key 'averageScore'.
+    """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$scores.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ]
+    return list(mongo_collection.aggregate(pipeline))
+
+
+if __name__ == "__main__":
+    pass
